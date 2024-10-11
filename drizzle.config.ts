2dad065c751ea4be86b80ff7a523261757cd8274
laurammoraesg4 <l.moraes@g4educacao.com>
2024-10-11 15:31:16 -0300

    adicionar o banco de dados em docker ao projeto

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   dialect: 'postgresql',
   dbCredentials: {
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
