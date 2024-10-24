9abfe60538db81f21081fb0ca7556a4d128c189e
laurammoraes2@gmail.com <laura@poontoagencia.com>
2024-10-23 21:29:05 -0300

    adicionar a estruturacao do banco de dados

--- src/database/schemas/schema.ts
+++ src/database/schemas/schema.ts
@@ -1,5 +1,3 @@
-
-
 import {  integer, pgTable, serial,  varchar, timestamp } from 'drizzle-orm/pg-core';
 
 
@@ -9,24 +7,29 @@ export const users = pgTable('users', {
   name: varchar('name', { length: 256 }),
   email: varchar('email', { length: 256}), 
   phone: varchar('phone', { length: 25}),
-  date_of_birth: varchar('date_of_birth', {length:25}),
+  dateOfBirth: varchar('date_of_birth', {length:25}),
   address: varchar('address', {length:256}),
   city: varchar('city', { length: 256}),
-  state: varchar('state', {length: 256})
+  state: varchar('state', {length: 256}),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at'),
+  deletedAt: timestamp('deleted_at'),
 });
 
 
 export const prescriptions = pgTable('prescriptions', {
-  id: serial('id').primaryKey(),
-  user_id: varchar('user_id', {length: 25}),
+  id: serial('id').primaryKey().notNull(),
+  userId: integer('user_id').notNull().references(() => users.id),
   type: varchar('type', { length: 256}),
-  medication_id: varchar('medication_id', {length: 256}),
-  dosage_frequency: integer('dosage_frequency'),
-  total_quantity: integer('total_quantity'),
-  alert_repurchase: integer('alert_repurchase'),
-  date_init: timestamp('date_init',{withTimezone: true,}),
-  date_end: timestamp('date_end',{withTimezone: true,}),
-
+  medicationId: integer('medication_id').notNull().references(() => medications.id),
+  dosageFrequency: integer('dosage_frequency'),
+  totalQuantity: integer('total_quantity'),
+  alertRepurchase: integer('alert_repurchase'),
+  dateInit: timestamp('date_init',{withTimezone: true,}),
+  dateEnd: timestamp('date_end',{withTimezone: true,}),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at'),
+  deletedAt: timestamp('deleted_at'),
 
 })
 
@@ -34,21 +37,27 @@ export const medications = pgTable('medications', {
   id: serial('id').primaryKey(),
   name: varchar('name', {length: 256}),
   dosage: varchar('dosage', {length: 256}),
-  brand_name: varchar('brand_name', {length: 256}),
+  brandName: varchar('brand_name', {length: 256}),
   type: varchar('type', {length: 256}),
-  total_quantity: varchar('total_quantity', {length: 256}),
-  form_of_use: varchar('form_of_use', {length: 256})
+  totalQuantity: varchar('total_quantity', {length: 256}),
+  formOfUse: varchar('form_of_use', {length: 256}),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at'),
+  deletedAt: timestamp('deleted_at'),
 })
 
 
 export const treatment_trackings= pgTable('treatment_trackings', {
-  id: serial('id').primaryKey(),
-  prescription_id: varchar('prescription_id', {length: 256}),
-  date_tracking: varchar('date_tracking', {length: 256}),
+  id: serial('id').primaryKey().notNull(),
+  prescriptionId: integer('prescription_id').notNull().references(() => prescriptions.id),
+  dateTracking: varchar('date_tracking', {length: 256}),
   status: varchar('status', {length: 256}),
-  brand_name: varchar('brand_name', {length: 256}),
+  brandName: varchar('brand_name', {length: 256}),
   type: varchar('type', {length: 256}),
-  total_quantity: varchar('total_quantity', {length: 256}),
-  form_of_use: varchar('form_of_use', {length: 256})
+  totalQuantity: varchar('total_quantity', {length: 256}),
+  formOfUse: varchar('form_of_use', {length: 256}),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at'),
+  deletedAt: timestamp('deleted_at'),
 })
 
